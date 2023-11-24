95627989a8a1d34e22c62db052a0eb4b2afee56b
shayan <sshayan961@gmail.com>
2023-11-24 17:27:19 +0500

    LawyerProfile changes

---
+++ src/main/java/Pages/LawyerProfile.java
@@ -0,0 +1,12 @@
+package Pages;
+
+import DriverFactory.DriverFact;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class LawyerProfile {
+    private WebDriver driver;
+}
--- src/test/java/StepDefinitions/LawyerProfile.java
+++ src/test/java/StepDefinitions/LawyerProfile.java
@@ -1,4 +1,18 @@
 package StepDefinitions;
 
+import DriverFactory.DriverFact;
+import Pages.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 public class LawyerProfile {
+
+    private static String Title;
+
+    @Given("user is on lawyerprofile page")
+    public void user_is_on_lawyerprofile_page() {
+        DriverFact.getDriver().get("https://makinglawsimple.staginganideos.com/lawyer-profile");
+
+    }
 }
